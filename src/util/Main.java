dd9bee5638240a06ad48c68603bf7122c057b09b
amomorning <amomorning@gmail.com>
2019-08-08 17:59:38 +0800

    update code

--- src/util/Main.java
+++ src/util/Main.java
@@ -24,9 +24,9 @@ public class Main extends PApplet {
 
 	float AA;
 	float BB;
-	
+
 	int color;
-	
+
 	boolean clicked = false;
 	public void setup() {
 		size(900, 900);
@@ -34,14 +34,14 @@ public class Main extends PApplet {
 		cp5 = new ControlP5(this);
 		cnt = 4;
 
-		cp5.addSlider("AA").setRange(0, 40000).setValue(25000).setPosition(20, 40);
+		cp5.addSlider("AA").setRange(0, 40000).setValue(25000).setPosition(20,
+				40);
 		cp5.addSlider("BB").setRange(0, 500).setValue(150).setPosition(20, 60);
 
-
 		Random rand = new Random(21239);
-		for(int i = 0; i < cnt; ++ i) {
-			int x = (rand.nextInt()%900+900)%900;
-			int y = (rand.nextInt()%900+700)%900;
+		for (int i = 0; i < cnt; ++i) {
+			int x = (rand.nextInt() % 900 + 900) % 900;
+			int y = (rand.nextInt() % 900 + 700) % 900;
 			System.out.println("x = " + x + " " + "y = " + y);
 			pts[i] = new WB_Point(x, y);
 		}
@@ -50,23 +50,21 @@ public class Main extends PApplet {
 	public void draw() {
 		background(0);
 
-		
-		if(clicked) {
+		if (clicked) {
 			drawBlock();
 		} else {
 			drawGradient();
 		}
-		
+
 		fill(255, 0, 0);
-//		for (int i = 0; i < cnt; ++i) {
-//			
-//			rect(pts[i].xf(), pts[i].yf(), 2f, 2f);
-//		}
-		block bb = new block(300, 300, 400);
-//		bb.draw(this, 0.3);
-//		
+		// for (int i = 0; i < cnt; ++i) {
+		//
+		// rect(pts[i].xf(), pts[i].yf(), 2f, 2f);
+		// }
+//		block bb = new block(300, 300, 400);
+		// bb.draw(this, 0.3);
+		//
 		cp5.draw();
-		
 
 	}
 
@@ -75,70 +73,69 @@ public class Main extends PApplet {
 				+ (y - pts[i].yd()) * (y - pts[i].yd());
 		return Math.sqrt(tmp);
 	}
-	
-	
+
 	public void mousePressed() {
 		int color = get(mouseX, mouseY) & 0xFF;
 		System.out.println(mouseX + " " + mouseY + " " + color);
 	}
-	
+
 	public double normalDistribution(double x) {
-		return 1.0/Math.sqrt(0.3*Math.PI)*Math.exp(-x*x/AA);
+		return 1.0 / Math.sqrt(0.3 * Math.PI) * Math.exp(-x * x / AA);
 	}
 	public void drawGradient() {
-				fill(255);
+		fill(255);
 		noStroke();
 		textSize(30);
 
 		for (int i = 0; i < 900; i += 2) {
 			for (int j = 0; j < 900; j += 2) {
-				int min = 255;
 				double total = 0;
 				for (int k = 0; k < cnt; ++k) {
 					double dis = distance(i, j, k);
 					if (dis != 0)
 						total += normalDistribution(dis);
 				}
-				color = (int) (BB*total);
+				color = (int) (BB * total);
 				if (color > 255)
 					color = 255;
 				fill(255 - color);
 				rect(i, j, 2, 2);
 
 				fill(0);
-				if(i%100 == 0 && j%100 == 0) {
-					text(" "+color, i, j);
+				if (i % 100 == 0 && j % 100 == 0) {
+					text(" " + color, i, j);
 				}
 			}
 		}
 	}
-	
+
 	public void drawBlock() {
 		int step = 60;
-		for(int i = 0; i < 900; i += step) {
-			for(int j = 0; j < 900; j += step) {
+		for (int i = 0; i < 900; i += step) {
+			for (int j = 0; j < 900; j += step) {
 				double total = 0;
-				for(int k = 0; k < cnt; ++ k) {
-					double dis = distance(i+step/2, j+step/2, k);
-					if(dis != 0) {
+				for (int k = 0; k < cnt; ++k) {
+					double dis = distance(i + step / 2, j + step / 2, k);
+					if (dis != 0) {
 						total += normalDistribution(dis);
 					}
 				}
-				double color = BB*total;
-				if(color > 255) color = 255;
-				double u = color/255;
-				
+				double color = BB * total;
+				if (color > 255)
+					color = 255;
+				double u = color / 255;
+
 				block bb = new block(i, j, step);
 				bb.draw(this, u);
-				
+
 			}
 		}
 	}
 
 	public void keyPressed() {
-		if(key == 'a') {
+		if (key == 'a') {
 			clicked = !clicked;
 		}
 	}
-	
- }
+
+}
