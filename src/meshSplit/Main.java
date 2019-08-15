aabd7b028c769584b0257a366b4d664c88c19c64
amomorning <amomorning@gmail.com>
2019-08-16 01:37:22 +0800

    set holes on mesh surface and export model

--- src/meshSplit/Main.java
+++ src/meshSplit/Main.java
@@ -21,30 +21,31 @@ import wblut.geom.WB_Polygon;
 import wblut.geom.WB_Transform;
 import wblut.geom.WB_Vector;
 import wblut.hemesh.HEC_Polygon;
+import wblut.hemesh.HET_Export;
 import wblut.hemesh.HET_MeshOp;
 import wblut.hemesh.HE_Face;
 import wblut.hemesh.HE_Mesh;
 
 /**
- * @author amo Aug 12, 2019 
+ * @author amo Aug 12, 2019
  * 
  */
-public class Main extends PApplet{
+public class Main extends PApplet {
 
 	private Tools tools;
 	private PolyTrans polytrans;
 	private int len = 10000;
 	private int n;
-	
+
 	ArrayList<MeshBlock> meshblock;
 	WB_Polygon ply, hole;
-	
+
 	HE_Mesh mesh;
-	
+
 	Gradient grad;
-	
-	boolean isHole = false;
-	
+
+	boolean isHole = true;
+
 	public void setup() {
 
 		size(1600, 900, P3D);
@@ -54,96 +55,130 @@ public class Main extends PApplet{
 		grad = new Gradient(polytrans.feets);
 
 		IG.init();
-		
-		setMeshes();
-	
-		if(isHole) setHoles();
-		
-	}
-	
-	
-	public WB_Polygon createPolygonWithPolygonHoles(WB_Polygon ply, WB_Polygon... holes ) {
 
-		ArrayList<WB_Coord> points =  new ArrayList<>(ply.getPoints().toList());
+		setMeshes();
+
+		if (isHole)
+			setHoles();
+
+		toObj();
+	}
+
+	/**
+	 *
+	 * @param:
+	 * @return:void
+	 * @throws:
+	 */
+	private void toObj() {
+		List<HE_Mesh> outMesh = new ArrayList<>();
+		List<HE_Mesh> outMeshWithHole = new ArrayList<>();
+		for (MeshBlock b : meshblock) {
+			for (int i = 0; i < 5; ++i) {
+				outMesh.add(b.meshes[i]);
+			}
+			for (HE_Mesh mesh : b.resultMeshes) {
+				outMeshWithHole.add(mesh);
+			}
+		}
+		String path = "../model/";
+		String name = "outMesh";
+		String nameHole = "outMeshWithHole";
+		HET_Export.saveToOBJ(outMesh, path, name);
+		HET_Export.saveToOBJ(outMeshWithHole, path, nameHole);
+	}
+
+	public WB_Polygon createPolygonWithPolygonHoles(WB_Polygon ply,
+			WB_Polygon... holes) {
+
+		ArrayList<WB_Coord> points = new ArrayList<>(ply.getPoints().toList());
 		@SuppressWarnings("unchecked")
 		ArrayList<WB_Coord>[] innerpoints = new ArrayList[holes.length];
-		for(int i = 0; i < holes.length; ++ i) {
+		for (int i = 0; i < holes.length; ++i) {
 			WB_Polygon hole = holes[i];
-			ArrayList<WB_Coord> pts = new ArrayList<>(Arrays.asList(hole.getPoints().toArray()));
-//			Collections.reverse(pts);
+			ArrayList<WB_Coord> pts = new ArrayList<>(
+					Arrays.asList(hole.getPoints().toArray()));
+			// Collections.reverse(pts);
 			innerpoints[i] = pts;
 		}
-		return (new WB_GeometryFactory()).createPolygonWithHoles(points, innerpoints);
+		return (new WB_GeometryFactory()).createPolygonWithHoles(points,
+				innerpoints);
 	}
-	
 
-	/** 
-	*
-	* @param:
-	* @return:void
-	* @throws:
-	*/
+	/**
+	 *
+	 * @param:
+	 * @return:void
+	 * @throws:
+	 */
 	private void setMeshes() {
 		meshblock = new ArrayList<>();
-		for(WB_Polygon ply: polytrans.polygons) {
+		for (WB_Polygon ply : polytrans.polygons) {
 			meshblock.add(new MeshBlock(this, ply));
 		}
-	
+
 		List<Double> out = new ArrayList<>();
-		for(MeshBlock b:meshblock) {
+		for (MeshBlock b : meshblock) {
 			b.u = grad.calcPosition(b.getCenter());
 			out.add(b.u);
-			
+
 			b.setSegments();
 		}
 		Collections.sort(out);
 		System.out.println(Collections.min(out));
 		System.out.println(Collections.max(out));
-		
+
 	}
-	
+
 	private void setHoles() {
-		for(MeshBlock b:meshblock) {
-			b.setHoles(10);
+		int cnt = 0;
+//		meshblock.get(600).setHoles();
+		for (MeshBlock b : meshblock) {
+			b.setHoles();
+			System.out.println("Finished Mesh Panel #" + (cnt++));
 		}
 	}
 
 	public void draw() {
 		background(255);
 		tools.cam.drawSystem(len);
-		if(isHole) tools.cam.openLight();
-		scale(-1, 1, 1);
-		if(!isHole) polytrans.display(this.g);
-		
+		if (isHole) {
+			tools.cam.openLight();
+			for(MeshBlock b:meshblock) {
+				b.dispalyWithHole(this.g);
+			}
+		} else {
+			polytrans.display(this.g);
 
-		for(MeshBlock b:meshblock) {
-			b.display(this.g);
-//			break;
+			for (MeshBlock b : meshblock) {
+				b.display(this.g);
+				// break;
+			}
 		}
 
-//		noFill();
-//		stroke(255, 0, 0);
-//		tools.render.drawPolygonEdges(ply);
-//
-//		fill(0, 0, 255);
-//		noStroke();
-//		tools.render.drawFaces(mesh);
+		// noFill();
+		// stroke(255, 0, 0);
+		// tools.render.drawPolygonEdges(ply);
+		//
+		// fill(0, 0, 255);
+		// noStroke();
+		// tools.render.drawFaces(mesh);
 		tools.drawCP5();
 	}
-	
+
 	public void keyPressed() {
-		if(key == '1') {
+		if (key == '1') {
 			grad.setFunctionType(1);
-		} else if(key == '2') {
+		} else if (key == '2') {
 			grad.setFunctionType(2);
-		} else if(key == '3') {
+		} else if (key == '3') {
 			grad.setFunctionType(3);
-		} else if(key == '0') {
+		} else if (key == '0') {
 			grad.isMin = !grad.isMin;
 		}
 		setMeshes();
-		if(isHole) setHoles();
+		if (isHole)
+			setHoles();
 	}
-
 
 }
--- src/meshSplit/MeshBlock.java
+++ src/meshSplit/MeshBlock.java
@@ -17,6 +17,7 @@ import wblut.geom.WB_Polygon;
 import wblut.geom.WB_Segment;
 import wblut.geom.WB_Transform;
 import wblut.geom.WB_Vector;
+import wblut.hemesh.HEC_FromPolygons;
 import wblut.hemesh.HEC_Polygon;
 import wblut.hemesh.HET_MeshOp;
 import wblut.hemesh.HE_Face;
@@ -36,17 +37,18 @@ public class MeshBlock {
 	ArrayList<WB_Point> pts;
 	WB_Polygon ply;
 	HE_Mesh mesh;
+	HE_Mesh[] meshes;
 	List<HE_Mesh> resultMeshes;
 	List<WB_Segment> segs;
 	WB_Render3D render;
 	private static final WB_GeometryFactory gf = new WB_GeometryFactory();
 
 	int numberOfPoints;
-	
-	static double triLength = 60;
+
+	static double triLength = 100;
 
 	double u = 1;
-	
+
 	WB_Point[] innerP;
 
 	public MeshBlock(PApplet app, WB_Coord... points) {
@@ -83,59 +85,97 @@ public class MeshBlock {
 	}
 
 	public void display(PGraphics app) {
-		app.fill(getColor());
-		app.noStroke();
-//		render.drawFaces(resultMeshes);
+		for (int i = 0; i < 5; ++i) {
+			app.fill(getColor());
+			app.noStroke();
+			render.drawFaces(meshes[i]);
+			app.noFill();
+			app.stroke(0);
+			render.drawEdges(meshes[i]);
+		}
 
 		app.stroke(0, 0, 255);
-		render.drawSegment(segs);
-		
+		// render.drawSegment(segs);
+
 		app.fill(255, 0, 0);
 		render.drawPoint(innerP, 20);
-
 	}
+	
+	public void dispalyWithHole(PGraphics app) {
+		app.fill(getColor());
+		app.noStroke();
+		render.drawFaces(resultMeshes);
+		
+		app.noFill();
+		app.stroke(0);
+		render.drawEdges(resultMeshes);
+	}
+
 	public void setSegments() {
+		meshes = new HE_Mesh[5];
 		segs = new ArrayList<WB_Segment>();
 		innerP = new WB_Point[4];
-		
+
 		innerP[0] = new WB_Point();
 		double temp = 1.0;
-		for(int i = 0; i < 4; ++ i) {
-			innerP[0].addSelf(pts.get((i+1)%4).mul(temp));
+		for (int i = 0; i < 4; ++i) {
+			innerP[0].addSelf(pts.get((i + 1) % 4).mul(temp));
 			temp /= u;
 		}
-		innerP[0].mulSelf((1.0-1.0/u)/(1-temp));
+		innerP[0].mulSelf((1.0 - 1.0 / u) / (1 - temp));
 		System.out.println(innerP[0]);
 
-		
-		for(int i = 1; i < 4; ++ i) {
-			innerP[i] = innerP[i-1].sub(pts.get(i)).mul(u);
+		for (int i = 1; i < 4; ++i) {
+			innerP[i] = innerP[i - 1].sub(pts.get(i)).mul(u);
 			innerP[i].addSelf(pts.get(i));
-//			innerP[i] = new WB_Point();
+			// innerP[i] = new WB_Point();
 		}
-		
-		for(int i = 0; i < 4; ++ i) {
-			segs.add(new WB_Segment(innerP[i], innerP[(i+1)%4]));
+
+		for (int i = 0; i < 4; ++i) {
+			segs.add(new WB_Segment(innerP[i], innerP[(i + 1) % 4]));
 			segs.add(new WB_Segment(innerP[i], pts.get(i)));
 		}
-		
-//		innerP =  
-		
-//		for(int i = 0; i < 4; ++ i) {
-//			WB_Segment line = new WB_Segment(pts.get((i+1)%4), pts.get((i+2)%4));
-//			WB_Point A = pts.get(i);
-//			
-//			double len = (u*0.5+0.1)*line.getLength(); 
-//			System.out.println("edge length = " + len);
-//			
-//			for(int j = 1; j*triLength + triLength < len; ++ j) {
-//				
-//				WB_Point B = line.curvePoint(j*triLength/len);
-//				System.out.println(A + "\n" + B);
-//				segs.add(new WB_Segment(A, B));
-//			}
-//			
-//		}
+
+		// innerP =
+		List<WB_Polygon> plys = new ArrayList<>();
+
+		for (int i = 0; i < 4; ++i) {
+			WB_Segment line = new WB_Segment(pts.get(i), innerP[i]);
+			WB_Point A = pts.get(i + 1);
+
+			double len = line.getLength();
+			System.out.println("edge length = " + len);
+
+			List<WB_Point> ptt = new ArrayList<>();
+
+			for (int j = 1; j * triLength < len; ++j) {
+
+				WB_Point B = line.curvePoint(j * triLength / len);
+				System.out.println(A + "\n" + B);
+				segs.add(new WB_Segment(A, B));
+				ptt.add(B);
+			}
+
+			if (ptt.size() == 0) {
+				plys.add(new WB_Polygon(pts.get(i), A, innerP[i]));
+			} else {
+
+				plys.add(new WB_Polygon(pts.get(i), A, ptt.get(0)));
+				for (int j = 0; j < ptt.size() - 1; ++j) {
+					plys.add(new WB_Polygon(ptt.get(j), A, ptt.get(j + 1)));
+				}
+				plys.add(new WB_Polygon(ptt.get(ptt.size() - 1), A, innerP[i]));
+			}
+
+			HEC_FromPolygons creator = new HEC_FromPolygons(plys);
+			meshes[i] = new HE_Mesh(creator);
+
+		}
+		WB_Polygon square = new WB_Polygon(innerP);
+		HEC_Polygon creator = new HEC_Polygon(square, 0);
+		meshes[4] = new HE_Mesh(creator);
+		HET_MeshOp.splitFacesTri(meshes[4]);
+
 	}
 
 	public int getColor() {
@@ -146,46 +186,66 @@ public class MeshBlock {
 		HET_MeshOp.splitFacesTri(mesh);
 		HET_MeshOp.splitFacesTri(mesh);
 	}
-	
-	public void setHoles(double b) {
-		MeshSplit();
-		mesh.triangulate();
-		List<HE_Face> faces = mesh.getFaces();
-		resultMeshes = new ArrayList<>();
-		for(HE_Face f:faces) {
-			WB_Transform T = new WB_Transform(f.getFaceCenter(), f.getFaceNormal(), WB_Point.ORIGIN(), WB_Vector.Z());
-			WB_Transform rT = new WB_Transform(WB_Point.ORIGIN(), WB_Vector.Z(), f.getFaceCenter(), f.getFaceNormal());
 
-			WB_Polygon ply = new WB_Polygon(f.toPolygon().apply(T).getPoints());
-			WB_Polygon hole = Tools.JTSOffset(ply, (1-u)*40 + 20, 40*(1-u) + 10);
-			
-			ply = createPolygonWithPolygonHoles(ply, hole);
-			resultMeshes.add((new HE_Mesh(new HEC_Polygon(ply, 0))).apply(rT));
+	public void setHoles() {
+		// MeshSplit();
+		// mesh.triangulate();
+
+		resultMeshes = new ArrayList<>();
+		for (int i = 0; i < 5; ++i) {
+			List<HE_Face> faces = meshes[i].getFaces();
+			for (HE_Face f : faces) {
+				WB_Transform T = new WB_Transform(f.getFaceCenter(),
+						f.getFaceNormal(), WB_Point.ORIGIN(), WB_Vector.Z());
+				WB_Transform rT = new WB_Transform(WB_Point.ORIGIN(),
+						WB_Vector.Z(), f.getFaceCenter(), f.getFaceNormal());
+
+				WB_Polygon ply = new WB_Polygon(
+						f.toPolygon().apply(T).getPoints());
+				WB_Polygon hole;
+				if(i < 4) {
+					hole = Tools.JTSOffset(ply, (1 - u) * 40 + 5,
+						20 * (1 - u) + 10);
+				}else  {
+					hole = Tools.JTSOffset(ply, (1 - u) * 50 + 10,
+						40 * (1 - u) + 10);
+				}
+
+				ply = createPolygonWithPolygonHoles(ply, hole);
+				resultMeshes
+						.add((new HE_Mesh(new HEC_Polygon(ply, 0))).apply(rT));
+			}
 		}
 	}
-	
-	public WB_Polygon createPolygonWithPolygonHoles(WB_Polygon ply, WB_Polygon... holes ) {
-		if(holes == null || holes.length == 0) return ply;
 
-		ArrayList<WB_Coord> points =  new ArrayList<>(ply.getPoints().toList());
+	public WB_Polygon createPolygonWithPolygonHoles(WB_Polygon ply,
+			WB_Polygon... holes) {
+		if (holes == null || holes.length == 0)
+			return ply;
+
+		ArrayList<WB_Coord> points = new ArrayList<>(ply.getPoints().toList());
 		@SuppressWarnings("unchecked")
 		ArrayList<WB_Coord>[] innerpoints = new ArrayList[holes.length];
 		boolean flag = false;
-		for(int i = 0; i < holes.length; ++ i) {
+		for (int i = 0; i < holes.length; ++i) {
 			WB_Polygon hole = holes[i];
-			if(holes[i] == null) continue;
+			if (holes[i] == null)
+				continue;
 			flag = true;
-			ArrayList<WB_Coord> pts = new ArrayList<>(Arrays.asList(hole.getPoints().toArray()));
-//			Collections.reverse(pts);
+			ArrayList<WB_Coord> pts = new ArrayList<>(
+					Arrays.asList(hole.getPoints().toArray()));
+			// Collections.reverse(pts);
 			innerpoints[i] = pts;
 		}
-		if(flag) return gf.createPolygonWithHoles(points, innerpoints);
-		else return ply;
+		if (flag)
+			return gf.createPolygonWithHoles(points, innerpoints);
+		else
+			return ply;
 	}
-	
+
 	public WB_Point getCenter() {
 		return ply.getCenter();
-		
+
 	}
 
 }
